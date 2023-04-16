8a7bc7e1d46ab9eaed61b931dbbea1cba1f1d843
kubazuch <jakubzuchowski13@gmail.com>
2023-04-17 00:52:52 +0200

    Lab 2, isi, stage 3 & 4

--- sop2/lab2 tut/isi/client.c
+++ sop2/lab2 tut/isi/client.c
@@ -6,18 +6,18 @@
 #define DISH_LEN 8
 #define MAX_MSG 10
 
-void client_work(mqd_t *dishes, mqd_t wd) {
+void client_work(mqd_t dish, mqd_t wd) {
     int pid = getpid();
     char mypid[MSG_LEN];
     snprintf(mypid, MSG_LEN, "%d", pid);
-    if(TEMP_FAILURE_RETRY(mq_send(dishes[0], mypid, MSG_LEN, 0)))
+    if(TEMP_FAILURE_RETRY(mq_send(dish, mypid, MSG_LEN, 0)))
         ERR("mq_send");
 
     char wydanie[MSG_LEN];
     int times = 0;
     for(;;) {
         if (times >= 5) {
-            printf("Ide do baru mlecznego\n");
+            printf("[%d] Ide do baru mlecznego\n", pid);
             exit(EXIT_SUCCESS);
         }
 
@@ -31,7 +31,7 @@ void client_work(mqd_t *dishes, mqd_t wd) {
         {
             if(errno == ETIMEDOUT)
             {
-                printf("Ide do baru mlecznego\n");
+                printf("[%d] Ide do baru mlecznego\n", pid);
                 exit(EXIT_SUCCESS);
             }
             ERR("mq receive");
@@ -39,30 +39,35 @@ void client_work(mqd_t *dishes, mqd_t wd) {
 
         if(strcmp(mypid, wydanie) == 0)
             break;
-
+        printf("[%d] %s to nie ja!\n", pid, wydanie);
         sleep(1);
         times++;
     }
-    printf("MNIAM\n");
+    printf("[%d] MNIAM\n", pid);
 }
 
 int main(int argc, char **argv)
 {
-    mqd_t d[D_NUM];
+    srand(getpid());
+    mqd_t d;
     mqd_t wd;
 
     struct mq_attr attr;
     attr.mq_maxmsg = MAX_MSG;
     attr.mq_msgsize = MSG_LEN;
 
-    if ((d[0] = TEMP_FAILURE_RETRY(mq_open("/DANIE1", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
+    uint rand_order = rand() % D_NUM;
+    char dish_num[DISH_LEN];
+    snprintf(dish_num, DISH_LEN, "/DANIE%d", rand_order + 1);
+    if ((d = TEMP_FAILURE_RETRY(mq_open(dish_num, O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
         ERR("DANIE open in");
 
     if ((wd = TEMP_FAILURE_RETRY(mq_open("/WYDANIE", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
         ERR("mq open in");
 
+    printf("[%d] Danie %d\n", getpid(), rand_order+1);
     client_work(d, wd);
-    mq_close(d[0]);
+    mq_close(d);
     mq_close(wd);
 
     return EXIT_SUCCESS;
--- sop2/lab2 tut/isi/server.c
+++ sop2/lab2 tut/isi/server.c
@@ -6,38 +6,118 @@
 #define DISH_LEN 8
 #define MAX_MSG 10
 
-void server_work(mqd_t *dishes, mqd_t wd) {
+struct queue_data {
+    mqd_t *d;
+    mqd_t *wd;
+};
+
+volatile sig_atomic_t should_end = 0;
+void sigint_handler(int sig, siginfo_t *info, void *p)
+{
+    should_end = 1;
+}
+
+void mq_handler(int sig, siginfo_t *info, void *p)
+{
+    struct queue_data *data;
+
+    data = (struct queue_data *)info->si_value.sival_ptr;
+
+    static struct sigevent noti;
+    noti.sigev_notify = SIGEV_SIGNAL;
+    noti.sigev_signo = SIGRTMIN;
+    noti.sigev_value.sival_ptr = data;
+    if (mq_notify(*(data->d), &noti) < 0)
+        ERR("mq_notify");
+
     char ordered[MSG_LEN];
-    for(;;) {
-        if(TEMP_FAILURE_RETRY(mq_receive(dishes[0], ordered, MSG_LEN, 0)) < 0)
-            ERR("mq receive");
-        printf("Order received from %s\n", ordered);
-        if(TEMP_FAILURE_RETRY(mq_send(wd, ordered, MSG_LEN, 0)) < 0)
+    while (!should_end) {
+        if (mq_receive(*(data->d), ordered, MSG_LEN, NULL) < 1) {
+            if (errno == EAGAIN)
+                break;
+            else
+                ERR("mq_receive");
+        }
+        sleep(1);
+        if(TEMP_FAILURE_RETRY(mq_send(*(data->wd), ordered, MSG_LEN, 0)) < 0)
             ERR("mq send");
+        printf("%s\n", ordered);
+    }
+}
+
+void server_work(mqd_t *dishes, mqd_t wd) {
+    struct timespec req = {10, 0};
+    for(;;) {
+        if(nanosleep(&req, NULL) == 0 || should_end)
+            break;
+        else if (errno == EINTR)
+            continue;
+        else
+            ERR("nanosleep");
+    }
+    printf("Kończenie\n");
+    char ordered[MSG_LEN];
+    for(int i = 0; i < D_NUM; ++i) {
+        for (;;) {
+            if (TEMP_FAILURE_RETRY(mq_receive(dishes[i], ordered, MSG_LEN, NULL)) < 1) {
+                if (errno == EAGAIN)
+                    break;
+                else
+                    ERR("mq_receive");
+            }
+            printf("Nie realizuję zamówienia %s\n", ordered);
+        }
+    }
+
+}
+
+void create_queues(mqd_t* wd, mqd_t d[]) {
+    struct mq_attr attr;
+    attr.mq_maxmsg = MAX_MSG;
+    attr.mq_msgsize = MSG_LEN;
+
+    if ((*wd = TEMP_FAILURE_RETRY(mq_open("/WYDANIE", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
+        ERR("mq open in");
+
+    char dish_num[DISH_LEN];
+
+    static struct queue_data data[DISH_LEN];
+    static struct sigevent noti[DISH_LEN];
+    for(int i = 0; i < D_NUM; i++) {
+        snprintf(dish_num, DISH_LEN, "/DANIE%d", i + 1);
+        if ((d[i] = TEMP_FAILURE_RETRY(mq_open(dish_num, O_RDWR | O_NONBLOCK | O_CREAT, 0600, &attr))) == (mqd_t)-1)
+            ERR("DANIE open in");
+
+        data[i].d = &d[i];
+        data[i].wd = wd;
+
+        noti[i].sigev_notify = SIGEV_SIGNAL;
+        noti[i].sigev_signo = SIGRTMIN;
+        noti[i].sigev_value.sival_ptr = &(data[i]);
+        if (mq_notify(d[i], &(noti[i])) < 0)
+            ERR("mq_notify");
     }
 }
 
 int main(int argc, char **argv)
 {
-    mqd_t d[D_NUM];
-    mqd_t wd;
+    mqd_t wd, d[D_NUM];
 
-    struct mq_attr attr;
-    attr.mq_maxmsg = MAX_MSG;
-    attr.mq_msgsize = MSG_LEN;
+    set_handler(mq_handler, SIGRTMIN);
+    set_handler(sigint_handler, SIGINT);
 
-    if ((d[0] = TEMP_FAILURE_RETRY(mq_open("/DANIE1", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
-        ERR("DANIE open in");
-
-    if ((wd = TEMP_FAILURE_RETRY(mq_open("/WYDANIE", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
-        ERR("mq open in");
-
+    create_queues(&wd, d);
     server_work(d, wd);
 
-    mq_close(d[0]);
     mq_close(wd);
-    if(mq_unlink("/DANIE1"))
-        ERR("mq unlink");
+    char dish_num[DISH_LEN];
+    for(int i = 0; i < D_NUM; i++) {
+        mq_close(d[i]);
+        snprintf(dish_num, DISH_LEN, "/DANIE%d", i + 1);
+        if(mq_unlink(dish_num))
+            ERR("mq unlink");
+    }
+
     if(mq_unlink("/WYDANIE"))
         ERR("mq unlink");
 
