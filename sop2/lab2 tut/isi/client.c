3fb432de216c06d31952b4d84761f8ba536fef51
kubazuch <jakubzuchowski13@gmail.com>
2023-04-16 23:44:04 +0200

    Lab 2, isi, stage 2

--- sop2/lab2 tut/isi/client.c
+++ sop2/lab2 tut/isi/client.c
@@ -6,6 +6,46 @@
 #define DISH_LEN 8
 #define MAX_MSG 10
 
+void client_work(mqd_t *dishes, mqd_t wd) {
+    int pid = getpid();
+    char mypid[MSG_LEN];
+    snprintf(mypid, MSG_LEN, "%d", pid);
+    if(TEMP_FAILURE_RETRY(mq_send(dishes[0], mypid, MSG_LEN, 0)))
+        ERR("mq_send");
+
+    char wydanie[MSG_LEN];
+    int times = 0;
+    for(;;) {
+        if (times >= 5) {
+            printf("Ide do baru mlecznego\n");
+            exit(EXIT_SUCCESS);
+        }
+
+        struct timespec ts;
+        if (clock_gettime(CLOCK_REALTIME, &ts) < 0) {
+            ERR("clock_gettime()");
+        }
+
+        ts.tv_sec += 5;
+        if(TEMP_FAILURE_RETRY(mq_timedreceive(wd, wydanie, MSG_LEN, NULL, &ts)) < 0)
+        {
+            if(errno == ETIMEDOUT)
+            {
+                printf("Ide do baru mlecznego\n");
+                exit(EXIT_SUCCESS);
+            }
+            ERR("mq receive");
+        }
+
+        if(strcmp(mypid, wydanie) == 0)
+            break;
+
+        sleep(1);
+        times++;
+    }
+    printf("MNIAM\n");
+}
+
 int main(int argc, char **argv)
 {
     mqd_t d[D_NUM];
@@ -15,14 +55,15 @@ int main(int argc, char **argv)
     attr.mq_maxmsg = MAX_MSG;
     attr.mq_msgsize = MSG_LEN;
 
-    if ((d[0] = TEMP_FAILURE_RETRY(mq_open("/DANIE1", O_RDWR, 0600, &attr))) == (mqd_t)-1)
+    if ((d[0] = TEMP_FAILURE_RETRY(mq_open("/DANIE1", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
         ERR("DANIE open in");
 
-    char ordered[MSG_LEN];
-    snprintf(ordered, MSG_LEN, "%d", getpid());
-    if(TEMP_FAILURE_RETRY(mq_send(d[0], ordered, MSG_LEN, 0)) < 0)
-        ERR("mq send");
+    if ((wd = TEMP_FAILURE_RETRY(mq_open("/WYDANIE", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
+        ERR("mq open in");
+
+    client_work(d, wd);
     mq_close(d[0]);
+    mq_close(wd);
 
     return EXIT_SUCCESS;
 }
--- sop2/lab2 tut/isi/server.c
+++ sop2/lab2 tut/isi/server.c
@@ -7,7 +7,14 @@
 #define MAX_MSG 10
 
 void server_work(mqd_t *dishes, mqd_t wd) {
-
+    char ordered[MSG_LEN];
+    for(;;) {
+        if(TEMP_FAILURE_RETRY(mq_receive(dishes[0], ordered, MSG_LEN, 0)) < 0)
+            ERR("mq receive");
+        printf("Order received from %s\n", ordered);
+        if(TEMP_FAILURE_RETRY(mq_send(wd, ordered, MSG_LEN, 0)) < 0)
+            ERR("mq send");
+    }
 }
 
 int main(int argc, char **argv)
@@ -22,12 +29,16 @@ int main(int argc, char **argv)
     if ((d[0] = TEMP_FAILURE_RETRY(mq_open("/DANIE1", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
         ERR("DANIE open in");
 
-    char ordered[MSG_LEN];
-    if(TEMP_FAILURE_RETRY(mq_receive(d[0], ordered, MSG_LEN, 0)) < 0)
-        ERR("mq receive");
-    printf("Order received from %s\n", ordered);
+    if ((wd = TEMP_FAILURE_RETRY(mq_open("/WYDANIE", O_RDWR | O_CREAT, 0600, &attr))) == (mqd_t)-1)
+        ERR("mq open in");
+
+    server_work(d, wd);
+
     mq_close(d[0]);
+    mq_close(wd);
     if(mq_unlink("/DANIE1"))
+        ERR("mq unlink");
+    if(mq_unlink("/WYDANIE"))
         ERR("mq unlink");
 
     return EXIT_SUCCESS;
